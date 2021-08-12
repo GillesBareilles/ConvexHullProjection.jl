1d5177b9afee57d1a9e52d9b40429ce4cc761701
Gilles Bareilles <gilles.bareilles@protonmail.com>
2021-08-12 15:41:24 +0200

    --

--- src/ConvexHullProjection.jl
+++ src/ConvexHullProjection.jl
@@ -1,5 +1,16 @@
 module ConvexHullProjection
 
+using LinearAlgebra
+using LinearOperators
+using Krylov
+using LineSearches
+
 greet() = print("Hello World!")
+
+include("projection_convexhull.jl")
+include("projection.jl")
+
+export ConvexHull
+export projection_zero
 
 end # module
