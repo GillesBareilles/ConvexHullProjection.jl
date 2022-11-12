7bbfaeb3b3396eee24da1aeb865af7fc83afc33c
Gilles Bareilles <gilles.bareilles@protonmail.com>
2022-11-12 23:02:09 +0100

    more general quadratic on spectraplex

--- src/simplex/projection_simplexshadow.jl
+++ src/simplex/projection_simplexshadow.jl
@@ -1,31 +1,26 @@
 raw"""
     SimplexShadow{Tf}
 
-Models the convex hull of vectors gs.
+Models the convex hull of vectors contained in `P`, with shift `a`.
+Corresponds to minimizing $0.5 * |P * x|^2 + ⟨a, x⟩$ with $x$ in the simplex.
 """
 struct SimplexShadow{Tf} <: StructuredSet{Tf}
-    A::Matrix{Tf}
+    P::Matrix{Tf}
+    a::Vector{Tf}
 end
 
 
 ################################################################################
 ## Ambient representation
 ################################################################################
-f(ch::SimplexShadow, α, ::AmbRepr) = 0.5 * norm(ch.A * α)^2
+f(ch::SimplexShadow, α, ::AmbRepr) = 0.5 * norm(ch.P * α)^2 + dot(ch.a, α)
 
-∇f!(res, ch::SimplexShadow, α, ::AmbRepr) = (res .= ch.A' * ch.A * α)
+∇f!(res, ch::SimplexShadow, α, ::AmbRepr) = (res .= ch.P' * ch.P * α .+ ch.a)
 
-∇²f!(res, ch::SimplexShadow, x, d, ::AmbRepr) = (res .= ch.A' * ch.A * d)
+∇²f!(res, ch::SimplexShadow, x, d, ::AmbRepr) = (res .= ch.P' * ch.P * d)
 
 g(::SimplexShadow, α, ::AmbRepr) = sum(α) == 1 && sum(α .>= 0) == length(α)
 
-
-"""
-    prox_γg!(res, ch, α)
-
-Computes the prox of the indicator of the simplex, which amounts to projecting onto the simplex.
-
-"""
 function prox_γg!(res, ::SimplexShadow{Tf}, α, ::AmbRepr) where Tf
     M = project_simplex!(res, α)
     return M
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,19 +10,19 @@ problem in JuMP and solving with Mosek.
 function projection_zero_JuMP(set::ConvexHull{Float64})
     n, k = size(set.gs)
     QUIET = true
-	  model = Model(with_optimizer(Mosek.Optimizer;
-                                 QUIET,
-		                             MSK_DPAR_INTPNT_CO_TOL_DFEAS = 1e-12,
-		                             MSK_DPAR_INTPNT_CO_TOL_INFEAS = 1e-12,
-		                             MSK_DPAR_INTPNT_CO_TOL_MU_RED = 1e-12,
-		                             MSK_DPAR_INTPNT_CO_TOL_PFEAS = 1e-12,
-		                             MSK_DPAR_INTPNT_CO_TOL_REL_GAP = 1e-12))
+    model = Model(with_optimizer(Mosek.Optimizer;
+        QUIET,
+        MSK_DPAR_INTPNT_CO_TOL_DFEAS=1e-12,
+        MSK_DPAR_INTPNT_CO_TOL_INFEAS=1e-12,
+        MSK_DPAR_INTPNT_CO_TOL_MU_RED=1e-12,
+        MSK_DPAR_INTPNT_CO_TOL_PFEAS=1e-12,
+        MSK_DPAR_INTPNT_CO_TOL_REL_GAP=1e-12))
 
     α = @variable(model, α[1:k])
     η = @variable(model, η)
     gconvhull = sum(α[i] .* set.gs[:, i] for i in 1:k)
     @objective(model, Min, η)
-	  socpctr = @constraint(model, vcat(η, gconvhull) in SecondOrderCone())
+    socpctr = @constraint(model, vcat(η, gconvhull) in SecondOrderCone())
 
     @constraint(model, sum(α) == 1)
     cstr_pos = @constraint(model, α .>= 0)
@@ -30,7 +30,7 @@ function projection_zero_JuMP(set::ConvexHull{Float64})
     optimize!(model)
 
     if termination_status(model) ∉ Set([MOI.OPTIMAL, MOI.LOCALLY_SOLVED])
-      @debug "projection_zero_JuMP: subproblem was not solved to optimality" termination_status(model) primal_status(model) dual_status(model)
+        @debug "projection_zero_JuMP: subproblem was not solved to optimality" termination_status(model) primal_status(model) dual_status(model)
     end
 
     return value.(α)
